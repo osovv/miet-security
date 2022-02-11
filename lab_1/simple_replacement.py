8aea873bb813638e3d256c97d8434a672e4180f1
Aleksey Chendemerov <merkyzeze@tutanota.com>
2022-02-11 18:01:33 +0300

    LAB-1 Write cipher output directly to a file

--- lab_1/simple_replacement.py
+++ lab_1/simple_replacement.py
@@ -1,23 +1,19 @@
-
-# Usage example
-# `python simple_replacement.py --encrypt --message "hellorworld" --keyFile "key.txt"`
-# `python simple_replacement.py --decrypt --message --keyFile "key.txt"`
-
 import argparse
 
 def encrypt(message: str, key: dict[int, int]) -> str:
-  encrypted = map(lambda char: chr(int(key[ord(char)])), message)
-  return ''.join(encrypted)
+  encrypted = map(lambda char: key[char], message)
+  return encrypted
 
 def decrypt(message: str, key: dict[int, int]) -> str:
-  decrypted = map(lambda char: chr(key[ord(char)]), message)
-  return ''.join(decrypted)
+  decrypted = map(lambda char: key[char], message)
+  return decrypted
 
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--message')
+  parser.add_argument('--file')
   parser.add_argument('--keyFile')
+  parser.add_argument('--output')
   parser.add_argument('--encrypt', action='store_true')
   parser.add_argument('--decrypt', action='store_true')
   args = parser.parse_args()
@@ -26,15 +22,24 @@ def main():
   with open(args.keyFile) as file:
     key = file.readline()
 
-  message = args.message
   encrypting_key = {idx: int(value) for idx, value in enumerate(key.split(' '))}
   decrypting_key = {int(value): idx for idx, value in enumerate(key.split(' '))}
 
+  file_contents = None
+  encrypted = None
+  with open(args.file, 'rb') as file:
+    file_contents = file.read()
+
+  output = args.output
 
   if args.encrypt is True:
-    print(encrypt(message, encrypting_key))
+    encrypted = encrypt(file_contents, encrypting_key)
+    with open(output, "wb") as out:
+      out.write(bytearray(encrypted))
   elif args.decrypt is True:
-    print(decrypt(message, decrypting_key))
+    decrypted = decrypt(file_contents, decrypting_key)
+    with open(output, "wb") as out:
+      out.write(bytearray(decrypted))
   else:
     print('No action type was given')
 
